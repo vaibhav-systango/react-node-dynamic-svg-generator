92c56dbd6729234c8bef01fdd2288e6a85383c18
vaibhav <vaibhav@systango.com>
2019-08-02 17:23:29 +0530

    Show dynamic text in SVG.

--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -4,12 +4,12 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <svg height="100" width="400" xmlnsXlink="http://www.w3.org/1999/xlink">
+        <svg height="100" width="1000" xmlnsXlink="http://www.w3.org/1999/xlink">
           <a xlinkHref="" target="_blank">
-            <text x="50" y="50" fill="red">Hello</text>
+            <text x="50" y="50" fill="red">{this.props.data && this.props.data.displayText}</text>
           </a>
           <text x="0" y="100">
-            An SVG Animation
+            An SVG dnamic configurable demo using Node, express and React js, You can change query params value and ?displayText=text to see the changes.  
           </text>
         </svg>
       </div>
