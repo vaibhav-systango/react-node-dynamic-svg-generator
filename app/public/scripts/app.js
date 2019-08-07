9fe866290c0ec0fd36e46427a7f5557d50c0f9d2
vaibhav <vaibhav@systango.com>
2019-08-07 15:32:18 +0530

    Change SVG with title, header and sub header dynamic.

--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -2,17 +2,21 @@ import React from 'react';
 
 export default class App extends React.Component {
   render() {
+    const { data } = this.props;
+    const { header, title, subHeader } = data;
     return (
-      <div>
-        <svg height="100" width="1000" xmlnsXlink="http://www.w3.org/1999/xlink">
-          <a xlinkHref="" target="_blank">
-            <text x="50" y="50" fill="red">{this.props.data && this.props.data.displayText}</text>
-          </a>
-          <text x="0" y="100">
-            An SVG dnamic configurable demo using Node, express and React js, You can change query params value and ?displayText=text to see the changes.  
-          </text>
-        </svg>
-      </div>
+      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        width="400px" height="327px" viewBox="0 0 400 327" enableBackground="new 0 0 400 327" xmlSpace="preserve">
+        <rect fill="#DEF2FD" width="400" height="327" />
+        <polygon opacity="0.2" fill="#7DACDC" enableBackground="new" points="0,327 200,173.797 400,327 " />
+        <polygon opacity="0.2" fill="#7DACDC" enableBackground="new" points="91,327 291,280.292 491,327 " />
+        <polygon opacity="0.2" fill="#7DACDC" enableBackground="new" points="-101,327 99,201 299,327 " />
+        <g>
+          <text transform="matrix(1 0 0 1 121.5 57.875)" fill="#0093C0" fontSize="29.7691">{ header ? header : "React Node" }</text>
+          <text transform="matrix(1 0 0 1 38.5483 114.1104)" fill="#016082" fontSize="97.8796">{ title ? title : "Dynamic"}</text>
+          <text transform="matrix(1 0 0 1 41.5005 141.4561)" fill="#0093C0" fontSize="14.5332" letterSpacing="140">{ subHeader ? subHeader : "SVG"}</text>
+        </g>
+      </svg>
     )
   }
 }
